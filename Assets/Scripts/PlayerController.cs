de793d7e1ffc2c801da27f97f9414655bae483e8
marleeeeeey <marleeeeeey@gmail.com>
2025-02-20 11:27:02 +0300

    Complete pickup objects. Add cilynder. Add counter with no logic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject capsule;
     public float moveSpeed = 0;
     
     private Rigidbody rb;
+    private int counter;
     private float movementX;
     private float movementY;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        counter = 0;
     }
 
     void OnMove(InputValue movementValue)
@@ -25,5 +28,16 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * moveSpeed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            var targetColor = other.gameObject.GetComponent<Renderer>().material.color;
+            capsule.GetComponent<Renderer>().material.color = targetColor;
+            other.gameObject.SetActive(false);
+            counter++;
+        }
     }
 }
